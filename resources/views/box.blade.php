34cfed516512d836a80929a1f091894d0810cdf5
Wisnu Yogi <wisnuyogipamungkas@gmail.com>
2023-10-25 23:33:13 +0700

    Upload Pertama

---
+++ resources/views/box.blade.php
@@ -0,0 +1,10 @@
+@extends('barang')
+
+@section('judul_menu')
+ini section judul
+<br />
+@endsection
+
+@section("isi_menu")
+ini section isi menu
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\controllers\Box;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+
+});
+
+Route::get('/DEANKT', function () {
+    //return view('welcome');
+    echo "KEEP RESPECT EACH OTHER";
+});
+
+Route::get('/percobaan',[Box::class, 'coba']);
+Route::get('/box',[Box::class, 'index']);
